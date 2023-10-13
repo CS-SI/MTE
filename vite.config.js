d3ba465d25f6db84e7cbdb5b676db2026d9a4d7a
PUECH Fabien <fabien.puech@csgroup.eu>
2023-10-13 09:52:35 +0200

    chore(cfg): add script and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import geojson from 'rollup-plugin-geojson'
-
+import { visualizer } from 'rollup-plugin-visualizer'
 const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
     geojson({
       compact: true,
     }),
+    visualizer(),
   ],
   resolve: {
     alias: {
